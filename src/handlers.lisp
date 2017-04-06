0641d1f248d7bc81e5529ca8f02e4b88eae71749
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-06 01:13:24 +0100

    decided to change the routes scheme

--- src/handlers.lisp
+++ src/handlers.lisp
@@ -54,48 +54,37 @@
      (:a :href "/angular/expressions/1" "Expressions 1") (:br)
      )))
 
-(defun tutorials-layout (args view)
+;;; tutorial examples ----------------------------------------------------------
+
+(defun tutorials-layout (view)
   (setf (html-mode) :HTML5)
   (with-html-output-to-string (*standard-output* nil :indent T :prologue T)
     (:html (:head (:script :src "https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"))
            (:body
             (fmt "~A" view)))))
 
-(defun tutorials (args)
-  (let ((tut     (cdr (assoc ":tutorial" args :test 'equal)))
-        (section (cdr (assoc ":section"  args :test 'equal))))
+(defun tut-intro ()
+  (tutorials-layout
+   (with-html-output-to-string (*standard-output* nil :indent T)
      ;; (cerror "debugging session" "tried ~a" args)
-    (tutorials-layout
-     args
-     (cond
-       ((equal tut "intro")
-        (tutorial-intro))
-       ((and (equal tut "expressions") (equal section "1"))
-        (tutorial-expr1))
-       (T
-        "Error: section not implemented")))))
+     (:div :data-ng-app "myApp" :data-ng-controller "myCtrl"
+           "First Name:" (:input :type "text" :data-ng-model "firstName") (:br)
+           "Last Name:"  (:input :type "text" :data-ng-model "lastName")  (:br)
+           (:br)
+           (fmt "Full Name: {{ ~a }}" (ps (+ first-name " " last-name))))
+     (:script
+      (fmt "~%~A"
+           (ps
+             ;; last AngularJS Example from
+             ;; https://www.w3schools.com/angular/angular_intro.asp
+             (var app (chain angular (module "myApp" (array))))
+             (chain app (controller "myCtrl" (lambda ($scope)
+                                               (setf (@ $scope first-name) "John")
+                                               (setf (@ $scope last-name) "Doe")
+                                               (return undefined))))))))))
 
-(defun tutorial-intro ()
-  (with-html-output-to-string (*standard-output* nil :indent T)
-    ;; (cerror "debugging session" "tried ~a" args)
-    (:div :data-ng-app "myApp" :data-ng-controller "myCtrl"
-          "First Name:" (:input :type "text" :data-ng-model "firstName") (:br)
-          "Last Name:"  (:input :type "text" :data-ng-model "lastName")  (:br)
-          (:br)
-          (fmt "Full Name: {{ ~a }}" (ps (+ first-name " " last-name))))
-    (:script
-     (fmt "~%~A"
-          (ps
-            ;; last AngularJS Example from
-            ;; https://www.w3schools.com/angular/angular_intro.asp
-            (var app (chain angular (module "myApp" (array))))
-            (chain app (controller "myCtrl" (lambda ($scope)
-                                              (setf (@ $scope first-name) "John")
-                                              (setf (@ $scope last-name) "Doe")
-                                              (return undefined)))))))))
-
-
-(defun tutorial-expr1 ()
-  (with-html-output-to-string (*standard-output* nil :indent T)
-    (:div :ng-app ""
-          (:p (fmt "My first expression: {{ ~a }}" (ps (+ 5 5)))))))
+(defun tut-expr1 ()
+  (tutorials-layout
+   (with-html-output-to-string (*standard-output* nil :indent T)
+     (:div :ng-app ""
+           (:p (fmt "My first expression: {{ ~a }}" (ps (+ 5 5))))))))
--- src/routes.lisp
+++ src/routes.lisp
@@ -7,9 +7,11 @@
   (hunchentoot:create-prefix-dispatcher "/javascripts/"  'handlers::assets)
   (hunchentoot:create-prefix-dispatcher "/stylesheets/"  'handlers::assets)
   (hunchentoot:create-prefix-dispatcher "/img/"          'handlers::assets)
+
+  (hunchentoot:create-prefix-dispatcher "/angular/intro/all"     'handlers::tut-intro)
+  (hunchentoot:create-prefix-dispatcher "/angular/expressions/1" 'handlers::tut-expr1)
   ;; routes where we might need arguments passed by script-name
   (server:create-custom-dispatcher :get "/parenscripts/:file"         'handlers::parenscripts)
-  (server:create-custom-dispatcher :get "/angular/:tutorial/:section" 'handlers::tutorials)
   ;; (server:create-custom-dispatcher :get "/contacts/:action/:id" 'handlers::contacts)
   ;; regex routes
   ;; (hunchentoot:create-regex-dispatcher "\\A/about\\z" 'handlers::about)
