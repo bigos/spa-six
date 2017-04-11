949c28046aa972105b5efc94969f5dede4671f60
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-11 14:31:46 +0100

    created a macro with-html-string

--- src/about-page.lisp
+++ src/about-page.lisp
@@ -2,13 +2,18 @@
 
 ;;; this package uses Marco Antoniotti's project
 ;;; https://gitlab.common-lisp.net/xhtmlambda/XHTMLambda
+(defmacro with-html-string (body)
+  (let ((result (gensym "STREAM")))
+    `(with-output-to-string (,result)
+       (with-html-syntax-output (,result :syntax :standard :print-pretty T)
+         ,body))))
+
 (defun about ()
-  (with-output-to-string (stream )
-    (with-html-syntax-output (stream :syntax :standard :print-pretty t)
-      (body (:style "color: red")
-            (p ()
-               "Some text here"
-               (ul ()
-                   (li () "Line 1")))
-            (script () (parenscript:ps
-                         (alert (+ "Hello " 1))))))))
+  (with-html-string
+    (body (:style "color: red" :empty-attribute "")
+          (p ()
+             "Some text here"
+             (ul ()
+                 (li () "Line 1")))
+          (script () (parenscript:ps
+                       (alert (+ "Hello " 1)))))))
