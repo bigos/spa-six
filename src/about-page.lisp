022d81c2bd34495f6ecb8ffac3504131c2f733a7
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-11 13:26:03 +0100

    project attribution

--- src/about-page.lisp
+++ src/about-page.lisp
@@ -1,5 +1,7 @@
 (in-package :about-page)
 
+;;; this package uses Marco Antoniotti's project
+;;; https://gitlab.common-lisp.net/xhtmlambda/XHTMLambda
 (defun about ()
   (with-output-to-string (stream )
     (with-html-syntax-output (stream :syntax :standard :print-pretty t)
